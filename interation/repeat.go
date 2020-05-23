ae7ab5ce3564acba2547995b7da4b7beff782bac
masisiliani <marcela.silva@resultadosdigitais.com.br>
2020-05-23 19:38:35 -0300

    replaced the logic I writed by strings.Replace() func

--- interation/repeat.go
+++ interation/repeat.go
@@ -1,11 +1,17 @@
 package interation
 
+import "strings"
+
 // Repeat function return a char repeated 5 times
 func Repeat(char string, qttRepeat int) string {
-	var output string
-	for i := 0; i < qttRepeat; i++ {
-		output += char
-	}
 
-	return output
+	return strings.Repeat(char, qttRepeat)
+
+	// deprecated code.
+	// var output string
+	// for i := 0; i < qttRepeat; i++ {
+	// 	output += char
+	// }
+
+	// return output
 }
