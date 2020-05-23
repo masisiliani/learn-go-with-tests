0895830174d6e474b29dad8f2f20aa70b87ef3e7
masisiliani <marcela.silva@resultadosdigitais.com.br>
2020-05-23 20:23:34 -0300

    Change Sum() func interface to receive slice parameter. Changed tests too.

--- arrayslices/sum.go
+++ arrayslices/sum.go
@@ -1,7 +1,7 @@
 package main
 
 // Sum function receive an array with 5 numbers and return the sum result
-func Sum(numbers [5]int) int {
+func Sum(numbers []int) int {
 	result := 0
 
 	for _, number := range numbers {
--- arrayslices/sum_test.go
+++ arrayslices/sum_test.go
@@ -3,12 +3,24 @@ package main
 import "testing"
 
 func TestSum(t *testing.T) {
-	numbers := [5]int{1, 2, 3, 5, 8}
-	got := Sum(numbers)
-	expected := 19
 
-	if got != expected {
-		t.Errorf("expected %d but got %d, %v", expected, got, numbers)
-	}
+	t.Run("sucess to sum five numbers for array", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 5, 8}
+		got := Sum(numbers)
+		expected := 19
 
+		if got != expected {
+			t.Errorf("expected %d but got %d, %v", expected, got, numbers)
+		}
+	})
+
+	t.Run("sucess to sum tree numbers with slice", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+		got := Sum(numbers)
+		expected := 6
+
+		if got != expected {
+			t.Errorf("expected %d but got %d, %v", expected, got, numbers)
+		}
+	})
 }
