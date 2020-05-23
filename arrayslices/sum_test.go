170294d58e234445b67d1fd5baab7edd961ce301
masisiliani <marcela.silva@resultadosdigitais.com.br>
2020-05-23 20:32:29 -0300

    remove unnecessary test

--- arrayslices/sum_test.go
+++ arrayslices/sum_test.go
@@ -4,20 +4,10 @@ import "testing"
 
 func TestSum(t *testing.T) {
 
-	t.Run("sucess to sum five numbers for array", func(t *testing.T) {
+	t.Run("sucess to sum any size of slice", func(t *testing.T) {
 		numbers := []int{1, 2, 3, 5, 8}
 		got := Sum(numbers)
 		expected := 19
-
-		if got != expected {
-			t.Errorf("expected %d but got %d, %v", expected, got, numbers)
-		}
-	})
-
-	t.Run("sucess to sum tree numbers with slice", func(t *testing.T) {
-		numbers := []int{1, 2, 3}
-		got := Sum(numbers)
-		expected := 6
 
 		if got != expected {
 			t.Errorf("expected %d but got %d, %v", expected, got, numbers)
