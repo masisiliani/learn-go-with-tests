f4075a83d8c689395cbc3c66fcb753d7096215e7
masisiliani <marcela.silva@resultadosdigitais.com.br>
2020-05-23 18:34:23 -0300

    Init tests from integer tests

--- integers/adder_test.go
+++ integers/adder_test.go
@@ -1,6 +1,9 @@
 package integers
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestAdder(t *testing.T) {
 	got := Add(2, 2)
@@ -9,4 +12,12 @@ func TestAdder(t *testing.T) {
 	if got != expected {
 		t.Errorf("fails to sum two numbers: expected %d but got %d", expected, got)
 	}
+}
+
+// Add function should be sum two number
+// For to do it if we pass 1 and 6 as argument, we will recieve 7 as return
+func ExampleAdd() {
+	sum := Add(1, 6)
+	fmt.Println(sum)
+	// Output: 7
 }
