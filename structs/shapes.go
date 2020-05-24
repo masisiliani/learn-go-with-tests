ae1f5c58481a23257eb7f5d0cb14bb4771be5810
masisiliani <marcela.silva@resultadosdigitais.com.br>
2020-05-24 19:52:00 -0300

    Add interface Shape to cover Area() func and simplify tests. Add new test helper

--- structs/shapes.go
+++ structs/shapes.go
@@ -1,11 +1,34 @@
 package structs
 
+import "math"
+
+// Shape is an abstract interface for geometry shape
+type Shape interface {
+	Area() float64
+}
+
+// Rectangle is a struct with values of rectagle object
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
 // Perimeter calculate the perimeter of a rectangle given a height and width
-func Perimeter(width, height float64) float64 {
-	return 2 * (width + height)
+func (r Rectangle) Perimeter() float64 {
+	return 2 * (r.Width + r.Height)
 }
 
 // Area returns the area of a rectangle
-func Area(width, height float64) float64 {
-	return width * height
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+// Circle is a struct with values of circle object
+type Circle struct {
+	Radius float64
+}
+
+// Area returns the area of a rectangle
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
 }
--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -3,7 +3,8 @@ package structs
 import "testing"
 
 func TestPerimeter(t *testing.T) {
-	got := Perimeter(10.0, 10.0)
+	rectangle := Rectangle{10.0, 10.0}
+	got := rectangle.Perimeter()
 	expected := 40.0
 
 	if got != expected {
@@ -12,10 +13,26 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	got := Area(12.0, 6.0)
-	expected := 72.0
 
-	if got != expected {
-		t.Errorf("expected %.2f but got %.2f", expected, got)
+	checkArea := func(t *testing.T, shape Shape, expected float64) {
+		t.Helper()
+		got := shape.Area()
+
+		if got != expected {
+			t.Errorf("expected %.2f but got %.2f", expected, got)
+		}
 	}
+
+	t.Run("rectangles", func(t *testing.T) {
+		rectangle := Rectangle{12.0, 6.0}
+		expected := 72.0
+		checkArea(t, rectangle, expected)
+
+	})
+
+	t.Run("circles", func(t *testing.T) {
+		circle := Circle{10}
+		expected := 314.1592653589793
+		checkArea(t, circle, expected)
+	})
 }
