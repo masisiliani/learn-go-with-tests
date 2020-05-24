c91ece666a8cb4b28884a1f250cca6b57f1c7b2b
masisiliani <marcela.silva@resultadosdigitais.com.br>
2020-05-24 19:28:29 -0300

    Fix test and create Area() func

--- structs/shapes.go
+++ structs/shapes.go
@@ -4,3 +4,8 @@ package structs
 func Perimeter(width, height float64) float64 {
 	return 2 * (width + height)
 }
+
+// Area returns the area of a rectangle
+func Area(width, height float64) float64 {
+	return width * height
+}
--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -13,7 +13,7 @@ func TestPerimeter(t *testing.T) {
 
 func TestArea(t *testing.T) {
 	got := Area(12.0, 6.0)
-	expected := 72
+	expected := 72.0
 
 	if got != expected {
 		t.Errorf("expected %.2f but got %.2f", expected, got)
